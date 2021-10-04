5bbbe8be669d08572bdf8f6d144f7c223b240284
tasnia <tasnianabila1@gmail.com>
2021-10-04 12:55:44 -0400

    added another kitten

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import { useState } from 'react';
 import kitty from './kitty.png';
+import kitty2 from './kitten2.jpg';
 
 function App() {
   const [blessed, setBlessed] = useState("Boop the kitten for luck");
+  const [image, setImage] = useState(kitty);
 
   const handleClick = () => {
     setBlessed("💖Kitten has blessed you💖")
+    setImage(kitty2);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>{ blessed }</h1>
-        <img onClick={ handleClick } src={ kitty } alt="a kitten"></img>
+        <img onClick={ handleClick} src={ image } alt="a kitten"></img>
       </header>
     </div>
   );
